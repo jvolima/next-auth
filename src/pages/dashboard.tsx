14c1319b2b265beb7b787a44bbe300b49676ef36
jvolima <jvolima2004@gmail.com>
2022-03-16 16:10:34 -0300

    Tratamento de erros de autenticação no server side rendering

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -20,9 +20,8 @@ export default function Dashboard() {
 
 export const getServerSideProps: GetServerSideProps = withSSRAuth(async (context) => {
   const apiClient = setupApiClient(context);
-  const response = await apiClient.get("/me");
 
-  console.log(response.data);
+  const response = await apiClient.get("/me");
 
   return {
     props: {}
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosError } from "axios";
 import { parseCookies, setCookie } from "nookies";
 import { signOut } from "../contexts/AuthContext";
+import { AuthTokenError } from "./errors/AuthTokenError";
 
 
 let isRefreshing = false;
@@ -75,6 +76,8 @@ export function setupApiClient(ctx = undefined) {
       } else {
         if(process.browser) {
           signOut();
+        } else {
+          return Promise.reject(new AuthTokenError())
         }
       }
     }
