5a576aa4942856f9912f15bb68ebda0cb732234d
jvolima <jvolima2004@gmail.com>
2022-03-17 14:21:35 -0300

    Criação do hook useCan que verifica se o user pode acessar algo

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -4,9 +4,14 @@ import { AuthContext } from "../contexts/AuthContext"
 import { setupApiClient } from "../services/api";
 import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
+import { useCan } from "../hooks/useCan";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
+
+  const userCanSeeMetrics = useCan({
+    roles: ['administrator', 'editor']
+  })
  
   useEffect(() => {
     api.get("me").then(response => console.log(response))
@@ -14,7 +19,10 @@ export default function Dashboard() {
   }, [])
 
   return (
-    <h1>Dashboard: {user?.email}</h1>
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+      { userCanSeeMetrics && <div>Métricas</div> }
+    </>
   )
 }
 
